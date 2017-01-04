006d1faa90e4b772b8513a6bcc524a8f5fe4200b
Whitney Hochstein <whitney@tests-MacBook-Pro-2.local>
2017-01-04 17:18:02 -0600

    mobile navigation

--- site.js
+++ site.js
@@ -2,5 +2,7 @@ var $ = window.jQuery
 $(function () {
 $('.ham').on('click',function(){
   $(this).toggleClass('open')
+  $('.mobilenav').fadeToggle()
+  $('body').toggleClass('prevent-scroll')
 })
 })
